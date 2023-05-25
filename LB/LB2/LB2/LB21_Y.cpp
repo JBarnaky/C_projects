5a3e6cea5ab5ad508c3ba8a28eac88e610ab7459
Paul Yatsk <47531332+JBarnaky@users.noreply.github.com>
2023-05-25 14:55:30 +0300

    Update LB21_Y.cpp

--- LB/LB2/LB2/LB21_Y.cpp
+++ LB/LB2/LB2/LB21_Y.cpp
@@ -1,21 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <conio.h>
 #include <math.h>
 #include <locale.h>
 
-void main()
-	{
-		setlocale(LC_CTYPE, "Russian");
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
 
-		double z1, z2, b;
-		
-		puts("Введите b");
+    double b;
 
-		scanf_s("%lf", &b);
+    puts("Р’РІРµРґРёС‚Рµ b:");
+    scanf("%lf", &b);
 
-		z1=(sqrt(2*b+2*(sqrt(pow(b,2)-4))))/((sqrt(pow(b,2)-4)+b+2));
-		z2=1/(sqrt(b+2));
-		
-		printf("\n Результат: z1=%.4lf z2=%.4lf\n", z1, z2);
-		_getch();
-	}
+    double temp = sqrt(pow(b, 2) - 4) + b + 2;
+    double z1 = sqrt(2 * b + 2 * temp) / temp;
+    double z2 = 1 / sqrt(b + 2);
+
+    printf("Р РµР·СѓР»СЊС‚Р°С‚: z1=%.4lf z2=%.4lf\n", z1, z2);
+
+    return 0;
+}
